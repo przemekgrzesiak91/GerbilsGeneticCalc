1c8bbf9146d27927c127f0ab772bed93392fce0a
Przemyslaw Grzesiak <przemek.grzesiak91@gmail.com>
2021-03-28 09:42:17 +0200

    Update .py

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
 from flask import Flask, render_template, request
+import itertools
+from collections import Counter
 
 app = Flask(__name__)
+
+
+def genes_combinations(F_genotype, M_genotype):
+    # combine every gene of Father with Mother
+    print(F_genotype, M_genotype)
+
+    possible_genes = []
+    for i in range(0, len(F_genotype) - 1, 2):
+        genes = itertools.product(F_genotype[i:i + 2], M_genotype[i:i + 2])
+        possible_genes.append(genes)
+
+    return possible_genes
+
+
+def genotypes_combinations(all_genes):
+    all_genotypes = list(itertools.product(*all_genes))
+
+    # print(len(all_genotypes)) number of possible genotypes = 4096
+
+    summary_genotypes = dict(Counter(all_genotypes))
+    print(summary_genotypes)
+    # print(''.join((('A', 'A'), ('C', 'C'), ('D', 'D'), ('E', 'E'), ('G', 'G'), ('P', 'P'))))
+
+
 
 @app.route('/',methods=['GET','POST'])
 def index():
     if request.method == "POST":
-        FA1 = request.form['FA1']
-        print(FA1)
+        #father's genotype
+        F_genotype = request.form.getlist('F_genotype[]')
+        M_genotype = request.form.getlist('M_genotype[]')
+
+        all_genes = genes_combinations(F_genotype, M_genotype)
+        genotypes_combinations(all_genes)
 
     return render_template('app.html')
 
